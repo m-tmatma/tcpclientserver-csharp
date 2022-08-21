6789a84b535f34f6da5b5639607bde66536302aa
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-08-21 09:33:37 +0900

    テンプレート

---
+++ tcpserver-async/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tcpserver_async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
