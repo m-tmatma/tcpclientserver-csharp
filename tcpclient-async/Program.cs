2174d73c905e4aa1558a169121a87809bdf74297
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-08-21 16:46:49 +0900

    ひな形

---
+++ tcpclient-async/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tcpclient_async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
